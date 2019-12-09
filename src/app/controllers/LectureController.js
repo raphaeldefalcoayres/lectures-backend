1dd759da6d5415923f7ef857e65997cd2188809d
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-12-09 11:01:18 -0300

    comment the code and test

--- src/app/controllers/LectureController.js
+++ src/app/controllers/LectureController.js
@@ -4,14 +4,15 @@ class LectureController {
   async store(req, res) {
     const { data } = req.body;
 
-    const tracks = [];
-
     const start_hour = 9;
     const interval_hour = 12;
     const end_hour = 17;
 
+    const tracks = [];
+
     let time = start_hour * 60;
 
+    // function convert minutes in hours with format
     const formatTime = minutes => {
       const h = minutes / 60 || 0;
       const m = minutes % 60 || 0;
@@ -22,30 +23,39 @@ class LectureController {
         .format('hh:mmA');
     };
 
+    // function convert minutes in hours rounding to floor
     const getHour = minutes => {
       return Math.floor(minutes / 60);
     };
 
+    // function get time in string of example: 20min or lightning
     const getTimeInString = string => {
       let newTime = string.split(/(\d+|lightning)/);
       newTime = newTime[1].replace('lightning', 5);
       return parseInt(newTime, 10);
     };
 
+    // processes will only be executed if a valid date exists
     if (data) {
       let trackId = 0;
 
+      // set the initial object
       tracks[trackId] = { title: `Track ${trackId + 1}`, data: [] };
+
+      // list the lectures posted
       data.forEach((item, index) => {
+        // only after zero add time for next lecture
         if (index > 0) {
           time += getTimeInString(data[index - 1]);
         }
 
+        // if time equal interval hour then add 60mins and insert step lunch
         if (getHour(time) === interval_hour) {
           time += 60;
           tracks[trackId].data.push('12:00PM Lunch');
         }
 
+        // if time is longer than end hour then insert step networking event and create a new track
         if (getHour(time) > end_hour - 1) {
           tracks[trackId].data.push('05:00PM Networking Event');
           trackId += 1;
@@ -53,8 +63,10 @@ class LectureController {
           time = start_hour * 60;
         }
 
+        // add lecture in track with time and description
         tracks[trackId].data.push(`${formatTime(time)} ${item}`);
 
+        // if end list of lectures then insert step networking event
         if (data.length === index + 1 && getHour(time) === end_hour - 1) {
           tracks[trackId].data.push('05:00PM Networking Event');
         }
